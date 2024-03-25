ae58e91a5ee08b08b2c716068c73bc0d8d99cc26
xefili <henrycarl2007@gmail.com>
2024-03-25 20:16:42 +0100

    style MDX generated stuff + new font family

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./public/styles/*.css'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				mono: "JetBrains Mono"
+			}
+		},
 	},
 	plugins: [],
 }
