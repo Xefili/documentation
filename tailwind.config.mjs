7d0509ca0afcd6d90686b6029f177eaad426cb0e
xefili <henrycarl2007@gmail.com>
2024-03-27 19:47:15 +0100

    Redirect to actual content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./public/styles/*.css'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./public/styles/*.css', '.src/pages/.astro'],
 	theme: {
 		extend: {
 			fontFamily: {
